c0a46d181e4ab5590472b9986ef35dd76317b474
foysal-ahamed <md.foysal.ahamed.bd@gmail.com>
2017-10-21 22:23:29 +0600

    isotope update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,16 @@ $(document).ready(function ($) {
 	$('.masonry-items').masonry({
 		percentPosition: true,
 	});
+
+	// init Isotope
+	var $filters = $('.masonry-items').isotope({
+		// options
+	});
+	// filter items on button click
+	$('.filter-menu ul').on('click', 'li', function () {
+		var filttering = $(this).attr('data-filter');
+		$filters.isotope({
+			filter: filttering
+		});
+	});
 });
